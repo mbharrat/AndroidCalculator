89895cc5ad226e3601feb13a75034e81f1ae86d1
mbharrat <mbharrat@gmail.com>
2017-01-07 11:56:57 -0500

    fixed divide

---
+++ app/src/main/java/mooc/vandy/java4android/calculator/logic/Divide.java
@@ -0,0 +1,21 @@
+package mooc.vandy.java4android.calculator.logic;
+
+/**
+ * Perform the Divide operation.
+ */
+public class Divide {
+    //private var
+    private int result = 0;
+    private int rem = 0;
+    
+    public Divide(int one, int two){
+        result = one/two;
+        rem = one%two;
+    }
+    //override toString()
+    public String toString(){
+        String first =  String.valueOf(result);
+        String two = String.valueOf(rem);
+        return first+" R: "+two;
+    }
+}
